1b477b43c7ad44b7d8a6da4149af666abcc50b7f
Aline Goppinger <91920984+AlineGpp@users.noreply.github.com>
2025-05-29 14:39:39 -0300

    iweb aplication for cats
    iweb aplication for cats

---
+++ script.js
@@ -0,0 +1,44 @@
+function menuOnClick() {
+    const nav = document.querySelector('.menu');
+    nav.classList.toggle('open');
+}
+
+
+function getValue(event) {
+    event.preventDefault(); 
+    const nome = document.getElementById('nome').value;
+    alert('O nome do seu gato é: ' + nome);
+    const addnameInList = document.getElementById('addnameInList');
+    const li = document.createElement('li');
+    li.appendChild = nome;
+    addnameInList.appendChild(li);
+   
+}
+
+
+// const swiper = new Swiper('.swiper-container', {
+//   slidesPerView: 1,
+//   pagination: {
+//     el: '.swiper-pagination'
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+//   breakpoints: {
+//     767: {
+//       slidesPerView: 2,
+//       setWrapperSize: true
+//     }
+//   }
+// })
+
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
