f7491dec907348385d4ff88192fc4c9cca2910cf
Aline Goppinger <91920984+AlineGpp@users.noreply.github.com>
2025-05-31 20:58:02 -0300

    Add product data and enhance swiper functionality; update styles for menu and links

--- script.js
+++ script.js
@@ -1,45 +1,27 @@
 function menuOnClick() {
-    const nav = document.querySelector('.menu');
-    nav.classList.toggle('open');
+  const nav = document.querySelector(".menu");
+  nav.classList.toggle("open");
 }
-
 
 function getValue(event) {
-    event.preventDefault(); 
-    const nome = document.getElementById('nome').value;
-    alert('O nome do seu gato é: ' + nome);
-    const addnameInList = document.getElementById('addnameInList');
-    const li = document.createElement('li');
-    li.appendChild = nome;
-    addnameInList.appendChild(li);
-   
+  event.preventDefault();
+  const nome = document.getElementById("nome").value;
+  alert("O nome do seu gato é: " + nome);
+  const addnameInList = document.getElementById("addnameInList");
+  const li = document.createElement("li");
+  li.appendChild = nome;
+  addnameInList.appendChild(li);
 }
 
-
-// const swiper = new Swiper('.swiper-container', {
-//   slidesPerView: 1,
-//   pagination: {
-//     el: '.swiper-pagination'
-//   },
-//   mousewheel: true,
-//   keyboard: true,
-//   breakpoints: {
-//     767: {
-//       slidesPerView: 2,
-//       setWrapperSize: true
-//     }
-//   }
-// })
-
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
   loop: true,
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   slidesPerView: 1,
   spaceBetween: 20,
@@ -51,6 +33,67 @@ const swiper = new Swiper('.swiper-container', {
     900: {
       slidesPerView: 2,
       spaceBetween: 30,
-    }
-  }
+    },
+  },
 });
+document.addEventListener("DOMContentLoaded", function () {
+  mapProducts();
+});
+
+let produtosSwiper;
+
+function mapProducts() {
+  fetch("produtos.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const swiperWrapper = document.querySelector(
+        "#produtos-swiper .swiper-wrapper"
+      );
+      swiperWrapper.innerHTML = "";
+
+      data.produtos.forEach((product) => {
+        const slide = document.createElement("div");
+        slide.classList.add("swiper-slide");
+        slide.innerHTML = `
+      <div class="produto">
+        <h3>${product.titulo}</h3>
+        <img src="${product.imagem}" alt="${product.titulo}" width="256" height="256" class="img-produto">
+        <button class="btn-produto">
+          <a href="${product.link}" target="_blank">Ver Produto</a>
+        </button>
+      </div>
+    `;
+        swiperWrapper.appendChild(slide);
+      });
+
+      if (produtosSwiper) {
+        produtosSwiper.update();
+      } else {
+        produtosSwiper = new Swiper("#produtos-swiper", {
+          loop: true,
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+          slidesPerView: 1,
+          spaceBetween: 20,
+          breakpoints: {
+            600: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            900: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+          },
+        });
+      }
+    })
+
+    .catch((error) => console.error("Erro ao carregar produtos:", error));
+}
